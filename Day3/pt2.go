777d8af0aab9b154b68b9f98f36f013f978658a0
LucaCorra02 <119894551+LucaCorra02@users.noreply.github.com>
2022-12-05 22:20:51 +0100

    Add files via upload
    Advent Of Code day 3 ( da migliorare)

---
+++ Day3/pt2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	fmt.Println(leggiFile())
+}
+
+func leggiFile() int{
+	myScanner := bufio.NewScanner(os.Stdin)
+	var score int
+	var tmp []string
+
+	for myScanner.Scan(){
+		tmp=append(tmp, myScanner.Text())
+		if len(tmp)==3{
+			r:=tmp[0][contenuta(tmp)]
+			if unicode.IsUpper(rune(r)) {
+				score += int(r-'A') + 27
+			} else {
+				score += int(r-'a') + 1
+			}
+			tmp=nil
+		}
+	}
+	return score
+}
+
+func contenuta(slc []string)int{
+	for pos,car:=range slc[0]{
+		if strings.ContainsRune(slc[1],car) && strings.ContainsRune(slc[2],car){
+			return pos
+		}
+	}
+	return -1
+}
