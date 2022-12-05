8460307a08e4a68bddb45680bb6cc309198a959b
LucaCorra02 <119894551+LucaCorra02@users.noreply.github.com>
2022-12-05 21:26:48 +0100

    Add files via upload
    Day 1 advent of code. Il file contiene entrambi le parti

---
+++ Day1/pt1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(leggiFile())
+}
+
+func leggiFile() int {
+	scanner := bufio.NewScanner(os.Stdin)
+	var max, max2, max3, sum int
+
+	for scanner.Scan() {
+		str := scanner.Text()
+		if str == "" {
+			if sum > max {
+				max = sum
+			} else if sum > max2 {
+				max2 = sum
+			} else if sum > max3 {
+				max3 = sum
+			}
+			sum = 0
+		} else {
+			val, _ := strconv.Atoi(str)
+			sum += val
+		}
+	}
+	return max + max2 + max3
+}
