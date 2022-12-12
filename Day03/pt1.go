02b61dee3ac00cc68e176ad8f3edc4df237e0fb7
LucaCorra02 <119894551+LucaCorra02@users.noreply.github.com>
2022-12-12 22:47:50 +0100

    Add files via upload

---
+++ Day03/pt1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	fmt.Println(leggiFile2())
+}
+
+func leggiFile2() int {
+	myScanner := bufio.NewScanner(os.Stdin)
+	var score int
+
+	for myScanner.Scan() {
+		str := myScanner.Text()
+		r := str[strings.IndexAny(str[:len(str)/2], str[len(str)/2:])]
+		if unicode.IsUpper(rune(r)) {
+			score += int(r-'A') + 27
+		} else {
+			score += int(r-'a') + 1
+		}
+	}
+	return score
+}
